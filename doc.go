d7510682c24fa2f5f6eab7bb32372cf463151805
Flavius Aspra <flavius.as@gmail.com>
2014-03-23 11:13:40 +0100

    fixed package name

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package graphdb provides an embeddable hypergraph database.
+Package hypergraphdb provides an embeddable hypergraph database.
 
 Basics
 
@@ -10,7 +10,7 @@ Features
 Here be features.
 */
 
-package graphdb
+package hypergraphdb
 /*
 Grammar:
 
